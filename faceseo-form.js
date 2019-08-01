37ff3d862a92777ef015ca67c9eab81bed52ef1a
lebadieudanang <53521306+lebadieudanang@users.noreply.github.com>
2019-08-01 11:52:18 +0700

    Update faceseo-form.js

--- faceseo-form.js
+++ faceseo-form.js
@@ -140,7 +140,7 @@ jQuery("#myModalthanh").html(formdiv);
 function thanhxet(){
 
 if(submittedform) {
-	window.location="https://miquangngondanang.blogspot.com/";
+	window.location="https://qno.vn";
 	
 	 if(document.cookie.indexOf("adf") == -1)
                    {
